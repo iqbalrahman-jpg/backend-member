3b8c7896e0b98e2b80bede56915f6c31c0c460e2
Iqbal Rahman <iqbal070103@gmail.com>
2023-05-28 02:05:43 +0700

    2. Update 2

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/beranda', index);
 app.use('/users', users);
 app.use('/akun', akun);
 app.use('/akun/{id}', akun);
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Percobaan 4' });
 });
 
+router.get('/beranda', function(req, res, next){
+  res.render('index', { title: 'Beranda'});
+})
+
 module.exports = router;
