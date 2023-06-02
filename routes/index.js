a0435f842c183dfdebe36fa118f77dd031643541
Iqbal Rahman <iqbal070103@gmail.com>
2023-06-02 16:20:24 +0700

    3. Upload route /

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Percobaan 4' });
+  res.render('index', { title: 'Percobaan Connect Database' });
 });
 
 router.get('/beranda', function(req, res, next){
