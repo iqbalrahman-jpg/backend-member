69fe800bca7563621fa2ac3c2089a9092a454ece
Iqbal Rahman <iqbal070103@gmail.com>
2023-06-02 16:31:07 +0700

    4. Update koneksi db ke aws

--- library/database.js
+++ library/database.js
@@ -1,9 +1,9 @@
 let mysql = require('mysql');
  
 let connection = mysql.createConnection({
-   host:        'localhost',
-   user:        'root',
-   password:    '',
+   host:        'backend-instance-1-ap-southeast-2a.caxrajgkzonh.ap-southeast-2.rds.amazonaws.com',
+   user:        'admin',
+   password:    'admin123',
    database:    'member'
  });
 
