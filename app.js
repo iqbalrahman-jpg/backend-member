6260e279f491c56c35ae950c6a2afa696ea06e45
Iqbal Rahman <iqbal070103@gmail.com>
2023-06-02 19:11:44 +0700

    5.2 Update cors

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var paket = require('./routes/pesanan_paket');
 var bayar = require('./routes/bayar');
 
 var app = express();
+const cors = require('cors');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', index);
 app.use('/beranda', index);
